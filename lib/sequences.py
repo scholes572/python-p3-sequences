3aacc89ca85a2db48e8d8e0b353e13a7f65e9f00
Agostino <agostinoscholes572@gmail.com>
2025-08-13 23:31:07 +0300

    Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    sequence = [0]
+
+    if length > 1:
+        sequence.append(1)
+        while len(sequence) < length:
+            sequence.append(sequence[-1] + sequence[-2])
+
+    print(sequence)        
+    
